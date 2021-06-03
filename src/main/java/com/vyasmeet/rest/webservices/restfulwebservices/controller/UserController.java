567317e49c894202dd3f4e8188569654ff8c074d
Meet Vyas <meet.server08@gmail.com>
2021-06-03 14:49:45 +0530

    POST request added for saving user. Returning HTTP 201 - Created. Returning URI of new user in location in response headers.

--- src/main/java/com/vyasmeet/rest/webservices/restfulwebservices/controller/UserController.java
+++ src/main/java/com/vyasmeet/rest/webservices/restfulwebservices/controller/UserController.java
@@ -3,8 +3,11 @@ package com.vyasmeet.rest.webservices.restfulwebservices.controller;
 import com.vyasmeet.rest.webservices.restfulwebservices.dto.UserDto;
 import com.vyasmeet.rest.webservices.restfulwebservices.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,7 +28,17 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public void saveUser(@RequestBody User user) {
+    public ResponseEntity<Object> saveUser(@RequestBody User user) {
         User newUser = userService.save(user);
+        // Return the new URI back.
+        // with HTTP created status code.
+        // /users/{id}  newUser.getId()
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
