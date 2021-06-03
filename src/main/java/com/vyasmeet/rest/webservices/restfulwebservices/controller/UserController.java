28fbb96a85cb59b2c6f3a8cef7f87c71fc70e541
Meet Vyas <meet.server08@gmail.com>
2021-06-03 14:10:10 +0530

    POST request added for saving user.

--- src/main/java/com/vyasmeet/rest/webservices/restfulwebservices/controller/UserController.java
+++ src/main/java/com/vyasmeet/rest/webservices/restfulwebservices/controller/UserController.java
@@ -3,9 +3,7 @@ package com.vyasmeet.rest.webservices.restfulwebservices.controller;
 import com.vyasmeet.rest.webservices.restfulwebservices.dto.UserDto;
 import com.vyasmeet.rest.webservices.restfulwebservices.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,14 +13,19 @@ public class UserController {
     private UserDto userService;
 
     // GET /users
+    // GET /users/{id}
+    @GetMapping("/users/{id}")
+    public User findUserByID(@PathVariable int id) {
+        return userService.findWithID(id);
+    }
+
     @GetMapping("/users")
     public List<User> getUsers() {
         return userService.getAll();
     }
 
-    // GET /users/{id}
-    @GetMapping("/users/{id}")
-    public User findUserByID(@PathVariable int id) {
-        return userService.findWithID(id);
+    @PostMapping("/users")
+    public void saveUser(@RequestBody User user) {
+        User newUser = userService.save(user);
     }
 }
