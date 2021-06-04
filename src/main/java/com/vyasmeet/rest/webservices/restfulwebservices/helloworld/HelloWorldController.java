4b125c2f5860e947c6a7488b435519b5ed7dacfa
Meet Vyas <meet.server08@gmail.com>
2021-06-04 11:29:33 +0530

    Added Accept-Language support (Internationalization with Locale)

--- src/main/java/com/vyasmeet/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/vyasmeet/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,10 +1,19 @@
 package com.vyasmeet.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 //Controller
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
@@ -18,5 +27,14 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/{name}")
     public HelloWorldBean helloWorldWithPath(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello %s!",name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(
+            @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource
+                .getMessage("good.morning.message",
+                        null,
+                        LocaleContextHolder.getLocale());
     }
 }
